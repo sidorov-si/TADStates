496e077e0a86a3861f40cdea1cb850c17e18a111
sidorov-si <svet.sidorov@gmail.com>
2015-10-19 22:29:59 +0300

    Add multiexperimental TAD calling.

--- call_tads.py
+++ call_tads.py
@@ -3,17 +3,25 @@
 Generate BED files with TAD coordinates from contact matrices 
 (or one contact matrix). For TADs search TADbit library is used.
 TADs can be called from one Hi-C experiment or several Hi-C 
-experiments for the same chromosomes.
+experiments for the same chromosomes. In the latter case all
+experiments must have the same resolution, names of all contact 
+matrices for the same chromosome must begin with the same 
+chromosome ID (e. g., chr1 or chr01). If several input directories
+are set, then all of them must contain the same count of contact 
+matrix files for the same set of chromosomes.
+
+All files contained in the input directory or directories are 
+considered to be contact matrix files.
 
 Usage:
-  call_tads.py -m <contact_matrix> -r <matrix_resolution> [-c <chromosome_name> -t <thread_number> -o <output_directory] 
-  call_tads.py -d <input_directory> -r <matrix_resolution> [-n <track_name_for_all_TADs> -t <thread_number> -o <output_directory>] 
+  call_tads.py -m <contact_matrix_list> -r <matrix_resolution> [-c <chromosome_name> -t <thread_number> -o <output_directory] 
+  call_tads.py -d <input_directories_list> -r <matrix_resolution> [-n <track_name_for_all_TADs> -t <thread_number> -o <output_directory>] 
 
 Options:
   -h --help                     Show this screen.
   --version                     Show version.
-  -m <contact_matrix>           Matrix of contact counts (.mat-file).
-  -d <input_directory>          Directory with contact matrices (.mat-files).
+  -m <contact_matrix>           One contact matrix (.mat-file) or a list of contact matrices for one chromosome. Several contact matrix filenames must be separated with comma.
+  -d <input_directory>          Directory with contact matrices (.mat-files) of a list of directories with contact matrices. Several directory names must be separated with comma.
   -c <chromosome_name>          Name of the chromosome. Determined from matrix file by default.
   -r <matrix_resolution>        Matrix resolution.
   -t <thread_number>            Number of threads for TADbit. Default: 4.
@@ -53,9 +61,11 @@ from os import listdir
 from re import search
 
 
-def call_tads(matrix_filename, chrom_name):
-    print "Processing chromosome " + chrom_name + '...'
-    print "Contact matrix:" + matrix_filename
+def call_tads(matrix_filenames, chrom_name):
+    print "Call TADs for chromosome " + chrom_name + '...'
+    print "Contact matrices: "
+    for matrix_filename in matrix_filenames:
+        print matrix_filename
     chrom_number = search(r'\d+|X|Y', chrom_name).group(0)
     if len(chrom_number) == 1 and chrom_number != 'X' and chrom_number != 'Y':
         chrom_number = '0' + chrom_number
@@ -65,14 +75,23 @@ def call_tads(matrix_filename, chrom_name):
         
     matrix_basename = splitext(basename(matrix_filename))[0]
     output_txt_filename = join(txt_directory, chrom_id + '_TADs.txt')
+    print 'Output TXT file:', output_txt_filename
     output_bed_filename = join(bed_directory, chrom_id + '_TADs.bed')
+    print 'Output BED file:', output_bed_filename
     filename_list.append(output_bed_filename)
 
     # Call TADs and write their borders in TADbit text format and in BED format
     chrom = Chromosome(name=chrom_name)
-    chrom.add_experiment(chrom_name, hic_data=matrix_filename, resolution=matrix_resolution)
-    chrom.find_tad(chrom_name, n_cpus=thread_number)
-    chrom.experiments[chrom_name].write_tad_borders(savedata=output_txt_filename)
+    combined_experiment_name = 'batch'
+    experiment_names = []
+    for matrix_index, matrix_filename in enumerate(matrix_filenames):
+        experiment_name = splitext(basename(matrix_filename))[0] + '_' + str(matrix_index)
+        experiment_names.append(experiment_name)
+        combined_experiment_name += '_' + experiment_name 
+        chrom.add_experiment(experiment_name, hic_data=matrix_filename, \
+                             resolution=matrix_resolution)
+    chrom.find_tad(experiment_names, batch_mode = True, n_cpus=thread_number)
+    chrom.experiments[combined_experiment_name].write_tad_borders(savedata=output_txt_filename)
     with open(output_txt_filename, 'r') as src, open(output_bed_filename, 'w') as dst:
         track_line = 'track name="' + chrom_name + '_TADs" visibility=1 itemRgb="On"'
         dst.write(track_line + '\n')
@@ -97,6 +116,7 @@ def call_tads(matrix_filename, chrom_name):
             dst.write(bed_line + '\n')
     chrom_filename = join(tdb_directory, chrom_id + '.tdb')
     chrom.save_chromosome(chrom_filename, force=True)
+    print 'Finish.'
 
 
 def get_chrom_name(matrix_filename):
@@ -107,33 +127,36 @@ def get_chrom_name(matrix_filename):
 
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version='call_tads 0.5')
+    arguments = docopt(__doc__, version='call_tads 0.6')
     if arguments["-m"] != None:
-        matrix_filename = arguments["-m"]
-        if not exists(matrix_filename):
-            print "Error: Can't find file with contact matrix: no such file '" + \
-                  matrix_filename + "'. Exit.\n"
-            sys.exit(1)
-        if not isfile(matrix_filename):
-            print "Error: File with contact matrix must be a regular file. " + \
-                  "Something else given. Exit.\n"
-            sys.exit(1)        
+        matrix_filenames = arguments["-m"].split(",")
+        for matrix_filename in matrix_filenames:
+            if not exists(matrix_filename):
+                print "Error: Can't find file with contact matrix: no such file '" + \
+                      matrix_filename + "'. Exit.\n"
+                sys.exit(1)
+            if not isfile(matrix_filename):
+                print "Error: File with contact matrix must be a regular file. " + \
+                      "Something else given. Exit.\n"
+                sys.exit(1)        
         if arguments["-c"] != None:
             chrom_name = arguments["-c"]
         else:
             chrom_name = None
-        input_directory = None
-    else:
-        matrix_filename = None
+        input_directories = None
+    elif arguments["-d"] != None:
+        matrix_filenames = None
         chrom_name = None
-        input_directory = arguments["-d"].rstrip('/')
-        if not exists(input_directory):
-            print "Error: Can't find input directory: no such directory '" + \
-                  input_directory + "'. Exit.\n"
-            sys.exit(1)
-        if not isdir(input_directory):
-            print "Error: Input directory must be a directory:). Something else given. Exit.\n"
-            sys.exit(1)
+        input_directories = arguments["-d"].split(",")
+        for input_directory in input_directories:
+            input_directory = input_directory.rstrip('/')
+            if not exists(input_directory):
+                print "Error: Can't find input directory: no such directory '" + \
+                      input_directory + "'. Exit.\n"
+                sys.exit(1)
+            if not isdir(input_directory):
+                print "Error: Input directory must be a directory:). Something else given. Exit.\n"
+                sys.exit(1)
 
     try:
         matrix_resolution = int(arguments["-r"])
@@ -183,17 +206,35 @@ if __name__ == '__main__':
     if not exists(tdb_directory):
         makedirs(tdb_directory)
 
-    if matrix_filename != None: # there is only one contact matrix
+    if matrix_filenames != None: # there is only one chromosome to process
         if chrom_name == None:
-            chrom_name = get_chrom_name(matrix_filename)
-        call_tads(matrix_filename, chrom_name)
-    else: # there is a directory with matrices
-        files = listdir(input_directory)
-        for file in files:
-            if splitext(basename(file))[1] == '.mat':
-                matrix_file_full = join(input_directory, file)
-                chrom_name = get_chrom_name(matrix_file_full)
-                call_tads(matrix_file_full, chrom_name)
+            chrom_name = get_chrom_name(matrix_filenames[0])
+        call_tads(matrix_filenames, chrom_name)
+    else: # there is a directory with matrices or a list of directories with matrices
+        # prepare a list of contact matrix filenames for each chromosome
+        # all these list are saved in a list
+        prev_count = -1
+        prev_directory = ''
+        full_list_of_files = []
+        for directory in input_directories:
+            files = sorted(listdir(directory))
+            if prev_count != -1 and len(files) != prev_count:
+                print "Error: The number of contact matrices in directory " + directory + \
+                      " is not equal to the number of contact matricws in directory" + \
+                      prev_directory + ". All directories must contain the same number of " + \
+                      "contact matrices (files)."
+                sys.exit(1)
+            else:
+                prev_count = len(files)
+                prev_directory = directory
+                full_list_of_files.append(files)
+        full_list_per_chr = [list(chr_tuple) for chr_tuple in zip(*full_list_of_files)]
+        for matrix_filenames in full_list_per_chr: 
+            # call TADs for each chromosome using several experiments
+            matrix_filenames_full = [join(directory, file) for directory, file in \
+                                     zip(input_directories, matrix_filenames)]
+            chrom_name = get_chrom_name(matrix_filenames_full[0])
+            call_tads(matrix_filenames_full, chrom_name)
         # merge BED files for individual chromosomes in one BED file
         genome_bed_filename = join(bed_directory, 'All_TADs.bed')
         with open(genome_bed_filename, 'w') as dst:
@@ -205,3 +246,4 @@ if __name__ == '__main__':
                         if i == 0:
                             continue
                         dst.write(line)
+
